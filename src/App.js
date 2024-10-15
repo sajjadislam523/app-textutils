0bb3c8f3199312b7b6fa9523d2621cbabc3a6a84
sajjadislam523 <sajjad.islam523@gmail.com>
2024-10-15 20:23:27 +0600

    Made some improvement in dark theme and UI

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
-import ThemeSwitcher from './components/ThemeSwitcher';
+import TextForm from './components/TextForm';
+import React, { useEffect, useState } from "react";
 
 function App() {
+
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = () => {
+    const newMode = mode === "light" ? "dark" : "light";
+    setMode(newMode);
+    document.documentElement.setAttribute("data-theme", newMode);
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", mode);
+  }, [mode]);
+
 
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About" contactText="Contact" homeText="Home" /> */}
-      <Navbar title="TextUtils" />
-      <div className='container px-6 md:px-0 mx-auto'>
-        <ThemeSwitcher />
-        {/* <TextForm heading="Enter the text to analyze below" /> */}
-        <About />
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <div className='container px-6 mx-auto md:px-0'>
+        <TextForm heading="Enter the text to analyze below" />
+        {/* <About /> */}
       </div>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,39 +8,28 @@ export default function Navbar(props) {
 
     return (
         <>
-            <nav className='flex items-center justify-between py-4 md:px-11'>
+            <nav className='flex items-center justify-between py-4 navbar bg-base-300 md:px-11'>
                 <div className='px-4 md:px-0'>
                     <p className='text-2xl font-bold'>{props.title}</p>
                 </div>
 
-                {/* Search bar */}
-                <div className='relative flex items-center'>
-                    <input
-                        type="text"
-                        placeholder='Search'
-                        className='w-full px-4 py-2 transition duration-300 ease-in-out border border-gray-300 rounded-full shadow-sm outline-none focus:ring-2 focus:ring-blue-400'
-                    />
-                    <button className='absolute z-10 p-2 text-white transition duration-300 ease-in-out bg-blue-500 border-none rounded-full shadow-md hover:bg-blue-600 hover:text-white right-2 focus:outline-none'>
-                        <img width="15" height="15" src="https://img.icons8.com/material-sharp/24/chevron-right.png" alt="chevron-right" />
-                    </button>
-                </div>
-
                 {/* Desktop menu */}
                 <div className='hidden md:flex'>
-                    <ul className='flex items-center justify-center gap-4'>
+                    <ul className='flex items-center justify-between gap-8'>
                         <li><a href="/">{props.homeText}</a></li>
                         <li><a href="/">{props.aboutText}</a></li>
                         <li><a href="/">{props.contactText}</a></li>
                     </ul>
                 </div>
+                <input type="checkbox" onClick={props.toggleMode} className="toggle toggle-primary" defaultChecked />
 
                 {/* Hamburger menu button (Visible on smaller screens) */}
                 <button onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
                     className='relative z-50 block px-4 md:hidden focus:outline-none'>
                     <div className={`hamburger ${isMobileMenuOpen ? 'open' : ''}`}>
-                        <span className={`line top ${isMobileMenuOpen ? 'bg-white' : 'bg-black'}`}></span>
-                        <span className={`line middle ${isMobileMenuOpen ? 'bg-white' : 'bg-black'}`}></span>
-                        <span className={`line bottom ${isMobileMenuOpen ? 'bg-white' : 'bg-black'}`}></span>
+                        <span className={`line top ${isMobileMenuOpen ? 'bg-white' : (props.mode === 'dark' ? 'bg-white' : 'bg-black')}`}></span>
+                        <span className={`line middle ${isMobileMenuOpen ? 'bg-white' : (props.mode === 'dark' ? 'bg-white' : 'bg-black')}`}></span>
+                        <span className={`line bottom ${isMobileMenuOpen ? 'bg-white' : (props.mode === 'dark' ? 'bg-white' : 'bg-black')}`}></span>
                     </div>
                 </button>
             </nav>
